50a4ceeaca2b3606039b3df8ba3c514a33c1e515
Kiarash Sotoudeh <kiarashsotoudeh@gmail.com>
2024-02-19 00:51:43 -0500

    First commit for website

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="container mx-auto h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-center text-gray-800">...</h1>
+      <p className="mt-4 text-lg text-center text-gray-600">...</p>
+    </div>
+  );
+};
+
+export default HomePage;
